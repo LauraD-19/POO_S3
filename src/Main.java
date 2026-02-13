323f0a3c0d0ad65ede7bfc8304b95e57877a3b97
LauraC <ldcaperav@gmail.com>
2026-02-13 17:18:53 -0500

    CAMBIOS GIT

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Laura Daniela Capera Vasquez y programe en Java");
+        System.out.println("Comandos de git:");
+        System.out.println("1. git add .");
+        System.out.println("2. git commit -c ");
+        System.out.println("3. git push origin");
+        System.out.println("4. git switch");
 
     }
 }
